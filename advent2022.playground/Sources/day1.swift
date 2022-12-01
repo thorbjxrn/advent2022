49567f7092171facd1e7dcb4db1b3f0e1f8d4f5d
thorbjørn <tbonvik@gmail.com>
2022-12-01 22:30:34 +0100

    solved day 1 part 2

--- advent2022.playground/Sources/day1.swift
+++ advent2022.playground/Sources/day1.swift
@@ -1,11 +1,19 @@
 import Foundation
 
 public func day1(inputString: String) -> Int {
-    let numbers = inputString.components(separatedBy: "\n\n").compactMap {
-        String($0).components(separatedBy: "\n").compactMap{ Int($0) }.reduce(0, { x, y in
-            x + y
-        })
-    }
+    var numbers = parseInput(inputString: inputString)
 
-    return numbers.max()!
+    // return numbers.max()!
+
+    numbers.sort{ $0 > $1 }
+
+    return Array(numbers.prefix(3)).reduce(0, +)
+    
+}
+
+public func parseInput(inputString: String) -> [Int] {
+    let numbers = inputString.components(separatedBy: "\n\n").compactMap {
+        String($0).components(separatedBy: "\n").compactMap{ Int($0) }.reduce(0, +)
+    }
+    return numbers
 }
