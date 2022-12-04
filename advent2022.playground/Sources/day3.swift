c4f62d426b6a8c7b5916a0010cda6ccd7d59aef7
thorbjørn <tbonvik@gmail.com>
2022-12-04 15:39:09 +0100

    day 3 part 2 complete

--- advent2022.playground/Sources/day3.swift
+++ advent2022.playground/Sources/day3.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+private let GROUP_SIZE = 3
+
 public func day3(inputString: String) -> Int {
     let numbers = parseInput(inputString: inputString)
     return numbers.reduce(0, +)
@@ -7,8 +9,9 @@ public func day3(inputString: String) -> Int {
 
 private func parseInput(inputString: String) -> [Int] {
     let lookupValues = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").enumerated()
-    let characters: [Character] = getDuplicates(from: inputString)
+    let characters: [Character] = getTriplets(from: inputString)
     return characters.compactMap({ char in
+        // print(char)
         for (n, x) in lookupValues {
             if (char == x) {
                 return n + 1
@@ -18,8 +21,40 @@ private func parseInput(inputString: String) -> [Int] {
     })
 }
 
-private func getTriplets(from string: String) -> Character {
-    return "f"
+private func getTriplets(from inputString: String) -> [Character] {
+    var strings: [String] = inputString.components(separatedBy: "\n")
+    var chars: [Character] = []
+
+    var index = 0
+
+    while !strings[0].isEmpty {
+        let group = [strings.removeFirst(), strings.removeFirst(), strings.removeFirst()]
+        chars.append(compareThreeStrings(strings: group))
+        index += GROUP_SIZE
+    }
+
+    return chars
+}
+
+private func compareThreeStrings(strings: [String]) -> Character {
+    guard strings.count == GROUP_SIZE, !strings[0].isEmpty, !strings[1].isEmpty, !strings[2].isEmpty else {
+        fatalError()
+    }
+
+    for char1 in Array(strings[0]) {
+        for char2 in Array(strings[1]) {
+            if char1 == char2{
+                for char3 in Array(strings[2]) {
+                    if char2 == char3 {
+                        print("\(strings) > \(char3)")
+                        return char3
+                    }
+                }
+            }
+        }
+    }
+
+    fatalError()
 }
 
 private func getDuplicates(from inputString: String) -> [Character] {
