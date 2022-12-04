6a2f41e3c61b37a2b481a12d69179ad7ded39179
thorbjørn <tbonvik@gmail.com>
2022-12-04 07:55:50 +0100

    solved day 3 part 1

--- advent2022.playground/Sources/day3.swift
+++ advent2022.playground/Sources/day3.swift
@@ -1,22 +1,44 @@
 import Foundation
 
 public func day3(inputString: String) -> Int {
-    var numbers = parseInput(inputString: inputString)
-
+    let numbers = parseInput(inputString: inputString)
     return numbers.reduce(0, +)
 }
 
 private func parseInput(inputString: String) -> [Int] {
     let lookupValues = Array("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ").enumerated()
-    print("lookupValues")
-
-    let numbers = inputString.components(separatedBy: "\n").compactMap { string as String in
-        let priority = {
-            let array = Array(string)
-            print(array.size)
-
-            array.makeIterator()
+    let characters: [Character] = inputString.components(separatedBy: "\n").compactMap { string in
+        guard !string.isEmpty else {
+            return nil
         }
+        let subject: Character = {
+            let array = Array(string)
+
+            let length = array.count
+            print(length)
+
+            var index = 0
+            while index < length/2 {
+                var secondIndex = length/2
+                while secondIndex < length {
+                    if(array[index] == array[secondIndex]){
+                        print(array[index])
+                        return array[index]
+                    }
+                    secondIndex += 1
+                }
+                index += 1
+            }
+            fatalError()
+        }()
+        return subject
     }
-    return numbers
+    return characters.compactMap({ char in
+        for (n, x) in lookupValues {
+            if (char == x) {
+                return n + 1
+            }
+        }
+        fatalError()
+    })
 }
