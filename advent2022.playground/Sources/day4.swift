1e72da0b0a4db86fa2b8a7752f1018d6a5f8a284
thorbjørn <tbonvik@gmail.com>
2022-12-16 11:41:20 +0100

    solve day 4

--- advent2022.playground/Sources/day4.swift
+++ advent2022.playground/Sources/day4.swift
@@ -1,21 +1,55 @@
 import Foundation
 
 public func day4(inputString: String) -> Int {
-    let strings = parseInput(inputString: inputString)
-    let count = 0
+    let ints = parseInput(inputString: inputString)
+    // print(ints)
 
-//    for string in strings {
-//        print(string)
-//    }
-
-    // return count
-    return -1
+    // return countEnclosed(integerArray: ints)
+    return countOverlapping(integerArray: ints)
 }
 
-private func parseInput(inputString: String) -> [String] {
+private func countEnclosed(integerArray:[[Int]]) -> Int {
+    var count = 0
+    for array in integerArray {
+        if array[1] < array[2] || array[0] > array[3] { //not neccessary but sligthly more efficient iteration
+            count += 0
+        }
+        else if array[0] >= array[2] && array[1] <= array[3] || array[2] >= array[0] && array[3] <= array[1] {
+            count += 1
+        }
+    }
+    return count
+}
+
+private func countOverlapping(integerArray:[[Int]]) -> Int {
+    var count = 0
+    for array in integerArray {
+        if array[1] < array[2] || array[0] > array[3] { //not neccessary but sligthly more efficient iteration
+            count += 0
+        }
+        else {
+            count += 1
+        }
+    }
+    return count
+}
+
+private func parseInput(inputString: String) -> [[Int]] {
     return inputString.components(separatedBy: "\n").compactMap({ string in
+        guard !string.isEmpty else {
+            return nil
+        }
+
+        let separators = CharacterSet(charactersIn: ",-")
+        let touple = string.components(separatedBy: separators).compactMap({ stringNumber in
+            return Int(stringNumber)
+        })
+        //let ranges = touple[0].components(separatedBy: "-"), touple[1].components(separatedBy: "-")
+        //let sum = Int(ranges[0])! - Int(ranges[1])!
+        //let result: String = "\()"
+
         //TODO: day 4
-        return ""
+        return touple
     })
 
 }
